9a34c5da15d5bac7d66445b6c2a712f521c9332a
NPotts24 <najaepretty@gmail.com>
2025-12-05 17:04:27 -0500

    adding login and signup pages

---
+++ app/src/main/java/com/example/tap2tell/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tap2tell
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // This connects the Kotlin code to the XML layout we will create next
+        setContentView(R.layout.activity_signup)
+    }
+}
